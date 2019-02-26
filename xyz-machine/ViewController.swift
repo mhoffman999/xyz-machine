a28c3215816ef665defccd0c8f856bc73fed5146
Michael Hoffman <mhoffman999@gmail.com>
2019-02-25 19:17:36 -0500

    Created IB and linked objects to VC file.

---
+++ xyz-machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Michael Hoffman on 2/25/19.
+//  Copyright © 2019 Here We Go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
